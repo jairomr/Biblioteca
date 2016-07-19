3719f7bf421c360e38fd5869fb7d399ee7ea7625
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2016-07-19 20:44:48 -0300

    Adcionado Comentarios
    Adicionei um comentario

--- application/models/Emprestimo.php
+++ application/models/Emprestimo.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Emprestimo extends CI_Model {
-
+        //Atributos
         private $idEmprestimo;
         private $dataEmprestimo;
         private $prazo;
