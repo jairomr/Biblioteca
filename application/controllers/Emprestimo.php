a67ebc44b575ab107018758141d589a72e60d9ba
Unknown <anacleiaferreirasilva@hotmail.com>
2016-10-01 10:13:01 -0300

    Desenvolvendo Emprestimo

---
+++ application/controllers/Emprestimo.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Emprestimo extends CI_Controller {
+
+	public function __construct() {
+        parent::__construct();
+        $this->load->helper(array('form'));
+        load_base('emprestimo');
+        $this->load->library('form_validation');
+    }
+	public function index()
+	{	
+		set_tema('titulo', 'Login');
+		$data= array(
+			'teste' => 'oi',
+			'teste2' => ENVIRONMENT);
+        set_tema('conteudo', load_conteudo('usuarios', 'cadastra', $data));
+        load_template();
+	}
+	public function cadastro()
+	{	
+		set_tema('titulo', 'Login');
+		$data= array(
+			'teste' => $this->uri->uri_string(),
+			'teste2' => ENVIRONMENT, );
+        set_tema('conteudo', load_conteudo('usuarios', 'cadastra', $data));
+        load_template();
+	}
+}
