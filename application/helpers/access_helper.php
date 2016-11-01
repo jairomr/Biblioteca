09e3d9952a5bdea9598356a0eae2e2f57e022dbf
Jairo Matos Da Rocha <contato@jairomr.com.br>
2016-11-01 21:35:59 -0200

    Update

--- application/helpers/access_helper.php
+++ application/helpers/access_helper.php
@@ -10,21 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @return type retorna a senha crypitada
  */
 function password($p) {
-    return base64_encode(pack('H*', sha1($p)));
-}
-function teemo($p) {
-   
-    return md5('ÇÂp7ãIn T3&m0'.
-        sha1(
-            md5('c4pItão-t&3mo'.
-                base64_encode(
-                    pack('H*', sha1('ajçiovfnwej'.$p)
-                        )
-                    ).
-                'n0-comando')
-            ).
-        'On D[]][Y');
-    
+    return sha1('23t_C\}*Q3ug&B`DGEKcMD\rk[w<+xW8NFz:Jf=F&e'.base64_encode(pack('H*', sha1($p))));
 }
 //.password   
 /**
@@ -54,68 +40,4 @@ function isLogado($redir = TRUE) {
 
 //.isLogado
 
-/**
- * ------------------------------
- * Metodo de verificar se é admin
- * ------------------------------
- * @param boolean $msg True para retornar a menssage de erro
- * @return boolean TRUE admin, False not-admin
- */
-function is_permission($nivel = 0, $msg = FALSE) {
-    $CI = & get_instance();
-    $CI->load->library('session');
-    if (isLogado(FALSE)) {
-        $admin = $CI->session->userdata('user_nivel');
-        if (!isset($admin) || $admin < $nivel) {
-            if ($msg) {
-                set_msg('msgerro', 'Seu usuario não tem permição para executar esta operação.', 'aviso');
-            }
-            return FALSE;
-        } else {
-            return TRUE;
-        }
-    } else {
-        if ($msg) {
-                set_msg('msgerro', 'Efetua seu login.', 'aviso');
-        }
-         return FALSE;
-    }
-}
-
-//.is_admin
-
-/**
- * ------------------------------
- * Metodo de retornar o login da sessao atual
- * ------------------------------
- * @return type login da sessao atual em toUpper
- */
-function login_name() {
-    $CI = & get_instance();
-    $login = $CI->session->userdata('user_login');
-    return ' ' . strtoupper($login);
-}
-
-//.login_name
-function nivelPermission($nivel){
-    switch ($nivel){
-        case 1:
-            return 'Registrado';
-        case 2:
-            return 'Assinante';
-        case 3:
-            return 'Colaborador';
-        case 4:
-            return 'Bolsista';
-        case 5:
-            return 'Pesquisador';
-        case 6:
-            return 'Editor';
-        case 7:
-            return 'Moderador';
-        case 8:
-            return 'Adminstrador';
-        case 9:
-            return 'WebMaster';
-    }
 }
--- application/models/Livro.php
+++ application/models/Livro.php
@@ -1,11 +1,27 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Modolo de livro
+ * Esta classe é um Model de livro projetado para o o aplicativo de biblioteca
+ * @author Ezequiel De Souza Feitosa <leo@genilhu.com> 
+ */
 class Livro extends CI_Model {
     public function __construct()
     {
                 // Call the CI_Model constructor
         parent::__construct();
     }
+    /**
+     * Registra um livro no banco de dados
+     * @param  date $data_registro Data do registro
+     * @param  String $titulo        Titulo do livro
+     * @param  String $edicao        Edição da livro
+     * @param  int $ano           Ano do lançamento
+     * @param  String $local_        Local da publicação
+     * @param  String $obs           Observação sobre o livro
+     * @param  String $editora       Editora que publico o livro
+     * @param  String $autor         Autor principal do livro
+     */
     public function registrar($data_registro ,$titulo, $edicao, $ano, $local_, $obs ,$editora,$autor ){
     	$dados=array(
     		'data_registro '=>$data_registro, 
@@ -19,22 +35,31 @@ class Livro extends CI_Model {
     		);
     	$this->db->insert('livros',$dados);
     }
+    /**
+     * Obenten toda as informação do livro
+     * @param  int $id Numero de indentificação do livro
+     * @return objeto     Retorna uma objeto com as informação do livro
+     */
     public function getById($id=null){
         if($id!=null){
-        $this->db->where('idlivros', $id);
-        return $this->db->get('livros');
+            $this->db->where('idlivros', $id);
+            return $this->db->get('livros');
         }
     }
+    /**
+     * Retorna todos os livros
+     * @return objeto Retorna um objeto com todos os livro
+     */
     public function getLivros(){
         return $this->db->get('livros');
     }
+    /**
+     * Atualiza os dados de um livro
+     * @param  int $id   Identificação do livro
+     * @param  array $data Lista com as Atualizaçãoes
+     */
     public function update($id,$data){
         $this->db->where('idlivros', $id);
         $this->db->update('livros',$data);
-
     }
-
-
-
-
 }
